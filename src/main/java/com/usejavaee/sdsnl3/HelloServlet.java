19b5189437f77af16e1c5030cda6eaf7a82f4611
tonrugeria <tonrugeria@gmail.com>
2021-09-09 15:00:59 +0800

    init

---
+++ src/main/java/com/usejavaee/sdsnl3/HelloServlet.java
@@ -0,0 +1,25 @@
+package com.usejavaee.sdsnl3;
+
+import java.io.*;
+import javax.servlet.ServletException;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+
+@WebServlet(name = "helloServlet", value = "/hello-servlet")
+public class HelloServlet extends HttpServlet {
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doPost(request, response);
+    }
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+
+        try (PrintWriter out = response.getWriter()) {
+            out.println("<h3> Hello World! I am Ton. </h3>");
+        }
+    }
+
+    public void destroy() {
+    }
+}
